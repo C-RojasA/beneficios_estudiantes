a211be7fe0464b14c753872906a01e41402358b8
C-RojasA <cristian@localhost.localdomain>
2017-01-11 02:01:28 -0300

    Refactoring project 	modified:   controllers/student.js 	modified:   index.js

--- controllers/student.js
+++ controllers/student.js
@@ -2,10 +2,25 @@
 
 const Student = require('./model/student')
 
-app.get('/api/student', (req, res) =>  {
-	res.send(200, {student})
-})
+function getStudents(req,res) {
+	Student.find({}, (err, students) => {
+		if (err) return res.status(500).send({message: `Error al realizar la peticion ${err}`})
+		if (!students) return res.status(500).send({message: `No existen datos almacenados` })
 
+		res.status(200).send({ students })		
+	})
+}
+
+function saveStudent6() {
+	let studentId = req.paramas.studentId
+
+	Student.findById(studentId, (err, student) => {
+		if(err) return status(500).send({message: `Error al realizar la peticion ${err}`})
+		if(!student) return status(500).send({message: `El Estudiante no Existe`})
+
+		res.status(200).send({student})
+	})
+}
 app.post('/api/student', (req, res) => {
 	console.log('POST /api/student')
 	console.log(req.body)
